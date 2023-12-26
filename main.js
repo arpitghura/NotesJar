c8f3c4812b14c840e9fc00f546f718690ed67d23
Preeti Kintali <preetikintala@gmail.com>
2023-12-26 18:02:23 +0530

    add note button enhanced

--- main.js
+++ main.js
@@ -147,3 +147,15 @@ function toastMessage(msg) {
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
   }
 addBtn.addEventListener("click", () => addNewNote());
+
+let spans = document.getElementsByClassName("circle");
+addBtn.onclick=function(){
+    for(span of spans){
+        span.classList.add("anim");
+    }
+    setTimeout(function(){
+        for(span of spans){
+            span.classList.remove("anim");
+        }
+    },500)
+}
